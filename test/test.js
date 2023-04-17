2964a789fc9011af0f7dfcdfbabb2161ca6e3cdd
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-04-17 12:53:29 +0300

    test

---
+++ test/test.js
@@ -0,0 +1,21 @@
+
+const axios = require('axios');
+const expect = require('chai').expect;
+
+describe('Add a new pet', function () {
+    it('Should add a new pet to the store', async function () {
+        const petData = {
+            id: 12345,
+            name: 'Fluffy',
+            photoUrls: ['https://www.example.com/fluffy.jpg'],
+            status: 'available'
+        };
+
+        const response = await axios.post('https://petstore.swagger.io/v2/pet', petData);
+
+        expect(response.status).to.equal(200);
+        console.log(response.status)
+        expect(response.data.name).to.equal('Fluffy');
+        expect(response.data.status).to.equal('available');
+    });
+});
